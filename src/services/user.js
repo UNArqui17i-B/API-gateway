414cd9291315c61b79a7ba172d1fd23233cf0ca4
maasencioh <maasencioh@gmail.com>
2017-04-10 22:42:06 -0500

    feat: auth_ms connection

--- src/services/user.js
+++ src/services/user.js
@@ -3,11 +3,12 @@
 const request = require('request-promise');
 const express = require('express');
 const status = require('http-status');
+const auth = require('../auth');
 
 module.exports = function (userServices) {
-    const userMicroService = `${userServices.userMicroService.host}:${userServices.userMicroService.port}/users`;
-    const authMicroService = `${userServices.authMicroService.host}:${userServices.authMicroService.port}/authentication`;
-    //const notificationMicroService = `${userServices.notificationMicroService.host}:${userServices.notificationMicroService.port}/notification`;
+    const userMicroService = `http://${userServices.userMicroService.host}:${userServices.userMicroService.port}/users`;
+    const authMicroService = `http://${userServices.authMicroService.host}:${userServices.authMicroService.port}/authentication`;
+    //const notificationMicroService = `http://${userServices.notificationMicroService.host}:${userServices.notificationMicroService.port}/notification`;
 
     const router = express.Router();
 
@@ -27,18 +28,53 @@ module.exports = function (userServices) {
 
     // Update a user
     router.put('/:id/:rev', function (req, res) {
+        auth(req.headers.authorization, authMicroService, (error) => {
+            if (!error && !error.error) {
+                request({
+                    method: 'PUT',
+                    url: `${userMicroService}/${req.params.id}/${req.params.rev}`,
+                    json: req.body
+                })
+                    .then((body) => res.status(status.OK).send(body))
+                    .catch((err) => res.status(status.BAD_REQUEST).send(err));
+            } else {
+                res.status(status.BAD_REQUEST).send(error);
+            }
+        });
+    });
+
+    // Delete a user
+    router.delete('/:id/:rev', function (req, res) {
+        auth(req.headers.authorization, authMicroService, (error) => {
+            if (!error && !error.error) {
+                request.delete(`${userMicroService}/${req.params.id}/${req.params.rev}`)
+                    .then((body) => res.status(status.OK).send(body))
+                    .catch((err) => res.status(status.BAD_REQUEST).send(err));
+            } else {
+                res.status(status.BAD_REQUEST).send(error);
+            }
+        });
+
+    });
+
+    // Check valid user
+    router.post('/login/:id', function (req, res) {
         request({
-            method: 'PUT',
-            url: `${userMicroService}/${req.params.id}/${req.params.rev}`,
+            method: 'POST',
+            url: `${authMicroService}/login/${req.params.id}`,
             json: req.body
         })
             .then((body) => res.status(status.OK).send(body))
             .catch((err) => res.status(status.BAD_REQUEST).send(err));
     });
 
-    // Delete a user
-    router.delete('/:id/:rev', function (req, res) {
-        request.delete(`${userMicroService}/${req.params.id}/${req.params.rev}`)
+    // Verifies user by email
+    router.put('/email/:id', function (req, res) {
+        request({
+            method: 'PUT',
+            url: `${authMicroService}/email/${req.params.id}`,
+            json: req.body
+        })
             .then((body) => res.status(status.OK).send(body))
             .catch((err) => res.status(status.BAD_REQUEST).send(err));
     });
